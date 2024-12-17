e02b3cb8cab3d3700088cfdccb69c8415dbd0de8
Rizsid <rizsid16@gmail.com>
2024-12-17 10:34:33 +0500

    remove custom css

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import './style.css';
 
-const CustomModal = ({ children, onClose, open = true, title = '' }) => {
+const CustomModal = ({ children, onClose, open = true, title = 'Update Note' }) => {
     return (
-        open && <div className='modal'>
-            <div className='modal_box'>
-                <i onClick={onClose} className="ri-close-large-fill cross_icon"></i>
-                {title && <h3 className='modal_title'> {title} </h3>}
+        open && <div className='fixed top-0 left-0 h-full w-full flex items-center justify-center bg-[rgba(0,0,0,0.2)]'>
+            <div className='min-h-[20%] w-[450px] bg-white shadow-md rounded-lg relative p-[2%] flex flex-col gap-3'>
+                <i onClick={onClose} className="ri-close-large-fill absolute top-[12px] right-[12px] cursor-pointer text-[20px]"></i>
+                {title && <h3 className='py-[12px]'> {title} </h3>}
                 {children}
             </div>
         </div>
